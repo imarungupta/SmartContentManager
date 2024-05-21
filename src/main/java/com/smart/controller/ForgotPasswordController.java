bf86c6553205c355ef26be4540658fe0a4921a4a
imarungupta <imarungupta@gmail.com>
2024-05-21 12:05:00 +0530

    Mail Service Implementation

--- src/main/java/com/smart/controller/ForgotPasswordController.java
+++ src/main/java/com/smart/controller/ForgotPasswordController.java
@@ -1,14 +1,21 @@
 package com.smart.controller;
 
+import com.smart.service.EmailService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.mail.MessagingException;
 import java.util.Random;
 
 @Controller
 public class ForgotPasswordController {
+
+    @Autowired
+    private EmailService emailService;
 
     @RequestMapping(value = "/forgotPassword")
     public String openForgotPasswordPage() {
@@ -18,7 +25,7 @@ public class ForgotPasswordController {
     Random random = new Random(1000);
 
     @PostMapping("/sendOTP")
-    public String sendForgotPasswordOTP(@RequestParam("email") String email) {
+    public String sendForgotPasswordOTP(@RequestParam("email") String email, HttpSession session) {
 
         System.out.println("Sending password for " + email);
         // Generate OTP of four digit
@@ -26,7 +33,21 @@ public class ForgotPasswordController {
         int Otp = random.nextInt(9999);
         System.out.println("OTP: " + Otp);
         // We have got OTP, now let's write code to send otp to user's email
+        String subject = "OTP From SCM.";
+        String message = "OTP- "+Otp;
+        String to = email;
+        try {
+            boolean sendEmailFlag = this.emailService.sendEmail(to, subject, email);
+            if(sendEmailFlag){
+                return "change_password";
+            }else {
+                session.setAttribute("message","Check Your Email ID");
+                return "verify_OTP";
+            }
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
 
-        return "verify_OTP";
+
     }
 }
